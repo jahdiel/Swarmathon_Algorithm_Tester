133530fc3e17c4ebb0e594c4483d23241dd364fe
BrianMunoz <brian.munoz1@upr.edu>
2017-02-08 18:50:11 -0400

    Implementation of Estrella Algorithm. Minor bug fixes(Inheritance from object for use of super()).

--- src/Estrella.py
+++ src/Estrella.py
@@ -7,16 +7,19 @@ class Estrella(Algorithm):
 
     def mainAlgorithm(self):
 
-        for rover in self.roverCluster.roverList:
-            rover.move()
-            t_coords, found = rover.searchTarget()
-            # if found:
-            #     target_found = self.targets.getTargetAtPos(t_coords)
-            #     if rover.pickTarget(target_found):
-            #         rover.returnToNest()
-            #
-            # if rover.isInNest():
-            #     target = rover.resetInNest()
-            #     if target is not None:
-            #         self.targets.removeTarget(target)
-            #         print 'Target List Size:', self.targets.size
+         for rover in self.roverCluster.roverList:
+             rover.move()
+             t_coords, found = rover.searchTarget()
+             if found:
+                 target_found = self.targets.getTargetAtPos(t_coords)
+                 if rover.pickTarget(target_found):
+                      rover.returnToNest()
+
+             if rover.isInNest():
+                 target = rover.resetInNest()
+                 if target is not None:
+                     self.targets.removeTarget(target)
+                     print 'Target List Size:', self.targets.size
+
+
+
--- src/RoverEstrellao.py
+++ src/RoverEstrellao.py
@@ -5,4 +5,4 @@ class RoverEstrellao(Rover):
     def __init__(self, CanvasWindow, pos_x, pos_y, number):
         super(RoverEstrellao, self).__init__(CanvasWindow, pos_x, pos_y, number)
 
-        self.velocity = 10
+        self.velocity = 8
